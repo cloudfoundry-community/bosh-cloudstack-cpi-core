01839a0345b387377184cee0b74a35e94d29380e
Pierre OBLIN <pierre.oblin@orange.com>
2015-06-09 19:04:53 +0200

    initial commit, based on spring boot starter (spring integration)

---
+++ src/main/java/com/orange/oss/cloudfoundry/BoshCloudstackCpiCore2Application.java
@@ -0,0 +1,12 @@
+package com.orange.oss.cloudfoundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoshCloudstackCpiCore2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoshCloudstackCpiCore2Application.class, args);
+    }
+}
---
+++ src/test/java/com/orange/oss/cloudfoundry/BoshCloudstackCpiCore2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orange.oss.cloudfoundry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BoshCloudstackCpiCore2Application.class)
+public class BoshCloudstackCpiCore2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
