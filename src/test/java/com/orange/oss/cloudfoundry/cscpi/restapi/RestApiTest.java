964c0ea429961942a32fc86f9c27591472b443cd
Pierre Oblin <pierre.oblin@orange.com>
2015-07-23 17:14:38 +0200

    add spring boot embedded tomcat integration test (validate the json cpi call and expected response from json data files)

--- src/test/java/com/orange/oss/cloudfoundry/cscpi/restapi/RestApiTest.java
+++ src/test/java/com/orange/oss/cloudfoundry/cscpi/restapi/RestApiTest.java
@@ -2,19 +2,30 @@ package com.orange.oss.cloudfoundry.cscpi.restapi;
 
 import java.io.IOException;
 
-import junit.framework.Assert;
+import static junit.framework.Assert.*;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
+import com.orange.oss.cloudfoundry.cscpi.BoshCloudstackCpiCoreApplication;
 
+/**
+ * Rest integration tests.
+ * see  http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html#boot-features-testing-spring-boot-applications
+ * @author pierre
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RestTestContext.class)
-@ConfigurationProperties
+@SpringApplicationConfiguration(classes = BoshCloudstackCpiCoreApplication.class)
+@WebIntegrationTest({"server.port=0", "management.port=0"})
 
 public class RestApiTest {
 	
@@ -25,15 +36,34 @@ public class RestApiTest {
 	}
 	
 	
+	//inject the dynamic http port of the embedded container
+	@Value("${local.server.port}")
+    int port;
+	
+	@Autowired
+	RestTemplate client;
+	
+	
 	@Test
 	public void testLoadJsonFromClasspath() throws IOException{
-		
 		String test="createvm";
-		
 		loadData(test);
+		
+	}
+	
+	
+	@Test
+	public void testCreateVM() throws IOException{
+		TestData data=this.loadData("createvm");
+
+		String response=this.client.postForObject("http://localhost:"+port+"/cpi", data.request, String.class);
+		assertEquals(data.response,response);
+		
 		
 		
 	}
+	
+	
 
 	/**
 	 * util class to load test data + expected data from classpath files
--- src/test/java/com/orange/oss/cloudfoundry/cscpi/restapi/RestTestContext.java
+++ src/test/java/com/orange/oss/cloudfoundry/cscpi/restapi/RestTestContext.java
@@ -1,9 +1,21 @@
 package com.orange.oss.cloudfoundry.cscpi.restapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestTestContext {
 
+	@Bean
+	public RestTemplate client(){
+		
+		
+		RestTemplate restTemplate=new RestTemplate();
+		//restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		return restTemplate;
+	}
+	
 }
