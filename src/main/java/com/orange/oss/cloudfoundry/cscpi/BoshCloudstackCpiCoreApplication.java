975cb58a40539d3f3b861c077c57f9813d862f32
Pierre Oblin <pierre.oblin@orange.com>
2015-07-31 11:59:44 +0200

    remove spring integration

--- src/main/java/com/orange/oss/cloudfoundry/cscpi/BoshCloudstackCpiCoreApplication.java
+++ src/main/java/com/orange/oss/cloudfoundry/cscpi/BoshCloudstackCpiCoreApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
 @ComponentScan(excludeFilters = @Filter(BeanMock.class))
-@IntegrationComponentScan       
+      
 public class BoshCloudstackCpiCoreApplication {
 
     public static void main(String[] args) {
