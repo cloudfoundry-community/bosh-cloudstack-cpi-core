b6f67b13a29c16fa7446b26243f0c6b71fb99b4c
Pierre Oblin <pierre.oblin@orange.com>
2015-08-18 18:41:11 +0200

    add cloud properties dns config to be set in cloudstack userData update TODO

--- src/test/java/com/orange/oss/cloudfoundry/cscpi/UserDataGeneratorTest.java
+++ src/test/java/com/orange/oss/cloudfoundry/cscpi/UserDataGeneratorTest.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.orange.oss.cloudfoundry.cscpi.domain.Network;
+import com.orange.oss.cloudfoundry.cscpi.domain.NetworkType;
+import com.orange.oss.cloudfoundry.cscpi.domain.Networks;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BoshCloudstackCpiCoreApplication.class)
@@ -19,7 +23,20 @@ public class UserDataGeneratorTest {
 	
 	@Test
 	public void testUserDataGeneration() {
-		String userData=this.generator.vmMetaData();
+
+		Networks networks=new Networks();
+		Network net=new Network();
+		networks.networks.put("default", net);
+		//net.type=NetworkType.dynamic;
+		net.type=NetworkType.manual;
+		net.ip="10.234.228.158";
+		net.gateway="10.234.228.129";
+		net.netmask="255.255.255.192";
+		net.cloud_properties.put("name", "3112 - preprod - back");
+		net.dns.add("10.234.50.180");
+		net.dns.add("10.234.71.124");
+		
+		String userData=this.generator.vmMetaData(networks);
 	}
 
 }
