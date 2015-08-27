f48a47bce8f2946fd66a79c2f9eb54ad89a01272
Pierre Oblin <pierre.oblin@orange.com>
2015-08-27 15:02:00 +0200

    get blobstore configuration from application.yml (ie from blobstore properties)

--- src/main/java/com/orange/oss/cloudfoundry/cscpi/config/DirectorConfig.java
+++ src/main/java/com/orange/oss/cloudfoundry/cscpi/config/DirectorConfig.java
@@ -7,13 +7,37 @@ import org.springframework.context.annotation.Configuration;
  * class to group properties from Director (ie: not passed to CPI verbs, got those props from director bosh release)
  *
  */
-//@Configuration
+@Configuration
 public class DirectorConfig {
 
-	@Value("${director.mbus}")
-	public String mbus;
+//	@Value("${director.mbus}")
+//	public String mbus;
 
+/**
+ * director props mapping
+ * blobstore:
+  provider: dav
+  options:
+    endpoint: http://10.234.228.157:25250
+    user: director
+    password: director-password
+	
+ */
+
+	@Value("${blobstore.provider}")
+	public String blobstore_provider;
+	
+	@Value("${blobstore.path}")
+	public String path;
 	
 	
+	@Value("${blobstore.options.endpoint}")
+	public String endpoint;
+
+	@Value("${blobstore.options.user}")
+	public String user;
+
+	@Value("${blobstore.options.password}")
+	public String password;
 	
 }
