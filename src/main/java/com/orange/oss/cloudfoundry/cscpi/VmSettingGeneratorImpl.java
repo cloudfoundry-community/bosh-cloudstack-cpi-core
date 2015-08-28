19b3186210ec00126c85a0efe277326cf65aabca
Pierre Oblin <pierre.oblin@orange.com>
2015-08-28 12:35:17 +0200

    fix typo

--- src/main/java/com/orange/oss/cloudfoundry/cscpi/VmSettingGeneratorImpl.java
+++ src/main/java/com/orange/oss/cloudfoundry/cscpi/VmSettingGeneratorImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.jclouds.cloudstack.domain.VirtualMachine;
-import org.neo4j.cypher.internal.compiler.v2_1.planner.logical.steps.aggregation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
