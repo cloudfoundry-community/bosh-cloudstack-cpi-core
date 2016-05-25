b8e34d2193d0b4575d131aa70042f4246d95572a
Pierre Oblin <pierre.oblin@orange.com>
2016-05-25 22:33:17 +0200

    fix registry client : set basic auth headers

--- src/main/java/com/orange/oss/cloudfoundry/cscpi/logic/BoshRegistryClientImpl.java
+++ src/main/java/com/orange/oss/cloudfoundry/cscpi/logic/BoshRegistryClientImpl.java
@@ -1,8 +1,15 @@
 package com.orange.oss.cloudfoundry.cscpi.logic;
 
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,39 +39,71 @@ public class BoshRegistryClientImpl implements BoshRegistryClient {
 	public void put(String vm_id, String settings) {
 
 		String uri = this.endpoint + "/instances/" + vm_id;
+		HttpHeaders headers=this.basicAuthEntityHeader();
+		
+		HttpEntity<String> request = new HttpEntity<String>(settings,headers);
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.postForObject(uri, settings, String.class);
-
+		restTemplate.exchange(uri,HttpMethod.POST,request, String.class);
 	}
 
 	@Override
 	public String get(String vm_id) {
 		String uri = this.endpoint + "/instances/" + vm_id + "/settings";
+		HttpHeaders headers=this.basicAuthEntityHeader();
+		
+		HttpEntity<String> request = new HttpEntity<String>(vm_id,headers);
 		RestTemplate restTemplate = new RestTemplate();
-		String result = restTemplate.getForObject(uri, String.class);
-		return result;
+		ResponseEntity<String> rep=restTemplate.exchange(uri,HttpMethod.GET,request, String.class);
+		return rep.getBody();
 	}
 
 	@Override
 	public String getRaw(String vm_id) {
 		String uri = this.endpoint + "/instances/" + vm_id + "/rawsettings";
-		RestTemplate restTemplate = new RestTemplate();
-		String result = restTemplate.getForObject(uri, String.class);
-		return result;
-	}
 
+		HttpHeaders headers=this.basicAuthEntityHeader();
+		
+		HttpEntity<String> request = new HttpEntity<String>(vm_id,headers);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> rep=restTemplate.exchange(uri,HttpMethod.GET,request, String.class);
+		return rep.getBody();
+	}
+	
 	@Override
 	public void delete(String vm_id) {
 		String uri = this.endpoint + "instances/" + vm_id;
+		HttpHeaders headers=this.basicAuthEntityHeader();
+		
+		HttpEntity<String> request = new HttpEntity<String>(vm_id,headers);
 		RestTemplate restTemplate = new RestTemplate();
+		
 		// Dont fail on exception (registry corrupt)
 		try {
-			restTemplate.delete(uri);
-
+			restTemplate.exchange(uri,HttpMethod.DELETE,request, String.class);
 		} catch (RestClientException e) {
 			logger.warn("could not delete vm {} from registry with exception {}. Ignoring ...",vm_id, e.getMessage());
 		}
 
 	}
 
+	/**
+	 * http header for registry basic authentication
+	 * @return
+	 */
+	private HttpHeaders basicAuthEntityHeader() {
+		byte[] bytes = String.format("%s:%s", this.user, this.password).getBytes();
+		String authHeader = String.format("Basic %s", Base64Utils.encodeToString(bytes));
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("AUTHORIZATION", authHeader);
+		
+		return headers;
+
+
+	}
+
+	
+	
+	
 }
